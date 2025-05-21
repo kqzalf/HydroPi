c21b9001dfa0ff0032efccc7811a5ed3ec6c6097
kqzalf <kqzalf@gmail.com>
2025-05-21 15:04:04 -0500

    resgr

--- rev1/Pi/Python/serialGrabber.py
+++ rev1/Pi/Python/serialGrabber.py
@@ -7,17 +7,17 @@ while True:
 
 	date = datetime.datetime.now().strftime("%m-%d-%y")
 	dt = datetime.datetime.now()
-	timeStamp =("{}.{:02d}".format(dt.strftime('%Y-%m-%d %I:%M:%S'), dt.microsecond//10000))
+	timeStamp = ("{}.{:02d}".format(dt.strftime('%Y-%m-%d %I:%M:%S'), dt.microsecond // 10000))
 
-	cc=str(ser.readline())
+	cc = str(ser.readline())
 	vars = cc[2:][:-3]
 	splitvars = vars.split("*")
 	
-	addphpexplode = str(splitvars[0] + '*' + splitvars[1] + '*' +splitvars[2] + '*' +splitvars[3] + '*' + str(timeStamp))
+	addphpexplode = str(splitvars[0] + '*' + splitvars[1] + '*' + splitvars[2] + '*' + splitvars[3] + '*' + str(timeStamp))
 	print(addphpexplode)
-	  f = open("/var/www/html/logs/mainlog.log","a") #opens file with name of "test.txt"
-        f.write("\n")
-        f.write(addphpexplode)
-        f.close()
+	f = open("/var/www/html/logs/mainlog.log", "a") #opens file with name of "test.txt"
+	f.write("\n")
+	f.write(addphpexplode)
+	f.close()
 
 	time.sleep(10)
